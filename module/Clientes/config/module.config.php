f3329ceb918ec853b77b298190bce78d0221d1e1
Ailton Martins <pinho123@gmail.com>
2017-07-27 12:54:27 -0300

    Criando modulo Clientes

---
+++ module/Clientes/config/module.config.php
@@ -0,0 +1,20 @@
+<?php
+  namespace Clientes;
+
+  return [
+      'controller' => [
+          'factories' => [
+
+          ]
+      ],
+
+      'router' => [
+
+      ],
+
+      'view_manager' => [
+          'template_path_stack' => [
+              'clientes'=>__DIR__."/../view"
+          ]
+      ]
+  ];
